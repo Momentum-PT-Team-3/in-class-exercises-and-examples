8984343647c496e3ab9de0b8dbda0498dc3cc3e2
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-11-17 16:14:14 -0500

    add horse exercise

---
+++ ex_horse.py
@@ -0,0 +1,24 @@
+'''For this exercise, you are going to write a program that models the 
+basketball shooting game, HORSE.
+
+- This game has two players.
+- Players alternate taking shots, and, if they miss, they get a letter.
+- For the purposes of this exercise, it will be random whether a player makes
+or misses a given shot.'''
+
+# Hints for getting started
+
+# What variables will you need for this program? In other words, what values
+# do you need to keep track of? 
+
+# What functions will you need? What, if any, input will each function require? 
+# What, if any, output will the function return? What, if any side effects will
+# the function have? For example, will any values be changed?
+
+# For Round 1, DO NOT WRITE ANY CODE. Use comments and psuedocode to plan out
+# what you are going to do. For example, you could do something like:
+# def shoot(player):
+#   determines whether player makes or misses shot
+#   updates players letters if missed
+
+# I will hit up each group individually after round one and get you set for round 2.
