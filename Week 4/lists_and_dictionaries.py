ac8ad7337da9eadde432a655b2e02b04107f730d
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-12-01 22:06:43 -0500

    update dict example

--- Week 4/lists_and_dictionaries.py
+++ Week 4/lists_and_dictionaries.py
@@ -40,10 +40,25 @@ def remove_non_string_data(list_of_stuff):
 
 cheeses = ['mozzarella', 'pepper jack', False, 'vegan cheese', 'cheddar']
 
-remove_non_string_data(cheeses)
+# remove_non_string_data(cheeses)
 
+bands = ["Jimmy Eat World", "Queen", "Andrew Bird", "The Band", "Led Zeppelin", 123, "The Band",]
 
+# dictionary where the keys are the and names and the values are info
+bands_dict = {
+    "Jimmy Eat World":{"members": 5},
+    "Queen": {"members": 4},
+    "Andrew Bird": {"members": 1},
+    "The Band": {"members": 4},
+    "Led Zeppelin": {"members": 4},
+}
 
+# add a band to this dictionary
+
+bands_dict["Mac Miller"] = {"members": 1}
+
+# obtain values by doing Name_of_dict["key"]
+print(f'The band "Led Zeppelin" has {bands_dict["Led Zeppelin"]["members"]} members.')
 
 
 
