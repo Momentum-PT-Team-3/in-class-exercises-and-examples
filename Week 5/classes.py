130bee837b8dc1ecece0b1f60f590583defb19b4
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-12-07 18:33:08 -0500

    add class example

--- Week 5/classes.py
+++ Week 5/classes.py
@@ -7,7 +7,56 @@
 
 def remove_short_words(word_list):
     for word in word_list:
-        if len(word) < 1:
-            word_list.pop()
+        if len(word) <= 1:
+            word_list.pop(word_list.index(word))
+    print(word_list)
     return word_list
-    
+
+test_list = ["words", "are", "simple", "a", "cat", "I"]
+
+remove_short_words(test_list)
+
+# try to represent a musician with a dictionary
+
+musicians = [{
+    "name": "Andrew Bird", 
+    "instrument": ["violin", "whistle"],
+    "record label": "Merge",
+    "bands" : []
+    },
+    {
+
+    }]
+
+# as we reach the limitations of dictionaries to represent data, we 
+# can move to classes
+# classes generally represent the nouns in our code, the people, places, and things
+
+class Musician:
+    def __init__(self, name, instrument):
+        self.name = name
+        self.instrument = instrument
+
+    def __str__(self):
+        return f'{self.name}: {self.instrument}'
+
+# when I create an object that's an element of a class, it's called an instance
+# let's create a musician and store that instance in a variable
+# writing the name of the class with () causes the __init__ method to be called
+snoop = Musician("Snoop Dogg", "MC")
+
+print(snoop)
+
+jewel = Musician("Jewel", "guitar")
+print(jewel)
+
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.members = []
+
+    def __str__(self):
+        return self.name
+
+mouse_rat = Band("Mouse Rat")
+print(f'The members of {band.name} are {mouse_rat.members}')
